540f76fa7ab9970889f8a3337245fa16e017631b
s24nath <bill24nath@gmail.com>
2022-04-22 00:54:42 +0530

    Testing Phase 2

--- scripts/script.js
+++ scripts/script.js
@@ -4,35 +4,57 @@ const display = document.getElementById('display');
 const resultBtn = document.getElementById('result-btn');
 const deleteBtn = document.getElementById('del');
 const topDisplay = document.getElementById('top-display');
+const dot = document.getElementById('dot');
+const allClear = document.getElementById('all-clear');
 let operand1;
 let operand2;
+let result;
+let lastOperation;
 let operator = '';
+let autoClearDisplay = false;
+let chainOperation = false;
 let operationWithResult = false;
 
 // Display 0 if the input is empty
 function displayZeroIfEmptyField() {
     display.value = display.value || 0;
-    display.value = 98;
-    console.log(display.value);
+}
+
+// Clear display
+function clearDisplay(confirm) {
+    if(confirm) {
+        // topDisplay.textContent = '';
+        display.value = '';
+        autoClearDisplay = false;
+    }
 }
 
 // Displaying numbers while clicking buttons
 function displayNum() {    
     for (const currentBtn of numericBtnList) {
         currentBtn.addEventListener('click',function () {
+            clearDisplay(autoClearDisplay);
             display.value += this.textContent;
+            chainOperation = true; 
+            operationWithResult = false;
         });
     }
 }
 
-// Restriction keyboard to only numbers
+// Restriction keyboard to only numbers and also restrict multiple dots if typed from keyboard.
 function onlyNum(e) {
     let getKey = e.key;
     if(!isNaN(Number(getKey)) || (getKey === '.' && !String(display.value).includes('.'))) {
+        clearDisplay(autoClearDisplay);
         return true;
     } else {
         return false;
     }
+}
+
+// Restriction multiple double if clicked from screen buttons
+function restrictMultiDot() {
+    display.value += !String(display.value).includes('.') ? '.' : '';
 }
 
 // Selection of operation clicked
@@ -44,6 +66,22 @@ function selectOperator() {
             topDisplay.textContent = `${operand1} ${operator}`;
         });
     }
+}
+
+// Assigning the first operand. Also used to chain operations.
+function assigningFirstOperand() {
+    if (operator !== '' && chainOperation) {
+        operand2 = Number(display.value);
+        operand1 = operation(operand1, operator, operand2);
+        display.value = operand1;
+        chainOperation = false;
+    } else if (operationWithResult) {
+        operand1 = result;
+    } else {
+        operand1 = Number(display.value);
+        chainOperation = false;
+    }
+    autoClearDisplay = true;
 }
 
 // Operations between operands
@@ -76,41 +114,43 @@ function operation(a,operatorSelected,b) {
 
 // Exponential operations
 function exponentialOperation(value,exponent) {
-    let result = 1;
+    let r = 1;
     for(let i = 1; i <= exponent; i++) {
-        result *= value;
+        r *= value;
     }
-    return result;
-}
-
-// Assigning the first operand. Also used to chain operations if the previous operator is not empty.
-function assigningFirstOperand() {
-    if(operator === '') {     
-        operand1 = Number(display.value); 
-        display.value = '';
-    } else {
-        operand2 = Number(display.value);
-        display.value = '',
-        operand1 = operation(operand1, operator, operand2);
-    }
+    return r;
 }
 
 // Displaying result 
 resultBtn.addEventListener('click', function () {
-    operand2 = Number(display.value);
-    display.value = operation(operand1, operator, operand2);
-    topDisplay.textContent = `${operand1} ${operator} ${operand2}`;
-    console.log(operand1);
-    console.log(operand2);
-    operator = '';
+    if(operator !== '') {        
+        operand2 = Number(display.value);
+        result = operation(operand1, operator, operand2);
+        display.value = result;
+        topDisplay.textContent = `${operand1} ${operator} ${operand2}`;
+        operator = '';
+        operationWithResult = true;
+        autoClearDisplay = true;
+    }
 });
 
 // Delete Last Character
 deleteBtn.addEventListener('click', function () {
     display.value = String(display.value).slice(0,-1);
+    // displayZeroIfEmptyField();
 });
 
-// displayZeroIfEmptyField();
+// All clear 
+allClear.addEventListener('click', function() {
+    // displayZeroIfEmptyField();
+    display.value = '';
+    topDisplay.textContent = '';
+    operator = '';
+    autoClearDisplay = true;
+    operationWithResult = false;
+});
+
+displayZeroIfEmptyField();
 assigningFirstOperand();
 selectOperator();
 displayNum();       
